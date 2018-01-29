90f0af9fde6955ba919f801c90b56b95531619c8
James Lee <jamesjlee8@gmail.com>
2018-01-28 22:02:39 -0500

    Added toolbar buttons and add tendies activity.

---
+++ app/src/main/java/co/crisdev/stocktendies/AddTendiesToPortfolio.java
@@ -0,0 +1,20 @@
+package co.crisdev.stocktendies;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+/**
+ * Created by lee on 1/28/18.
+ */
+
+public class AddTendiesToPortfolio extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_tendies_to_portfolio);
+
+    }
+}
--- app/src/main/java/co/crisdev/stocktendies/MainActivity.java
+++ app/src/main/java/co/crisdev/stocktendies/MainActivity.java
@@ -1,29 +1,67 @@
 package co.crisdev.stocktendies;
 
-import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.view.LayoutInflater;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Currency;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends ListActivity {
 
-    private ListView listView;
+    private ArrayList<Tender> tendiesList;
+    private SharedPreferences sharedPreferences;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add:
+                Intent intent = new Intent(this, AddTendiesToPortfolio.class);
+                startActivity(intent);
+                break;
+            case R.id.reset:
+                new AlertDialog.Builder(this)
+                        .setTitle("Reset Portfolio")
+                        .setMessage("Do you really want to drop all your tendies from your portfolio?")
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                Toast.makeText(MainActivity.this, "Dropped all your tendies.", Toast.LENGTH_SHORT).show();
+                                tendiesList.clear();
+                            }})
+                        .setNegativeButton(android.R.string.no, null).show();
+                break;
+            case R.id.about:
+
+                break;
+            default:
+                break;
+        }
+
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<Tender> tendiesList = new ArrayList<Tender>();
+        tendiesList = new ArrayList<Tender>();
         tendiesList.add(new Tender(getResources().getDrawable(R.drawable.ic_launcher_foreground), "Venus", 2.22, "$", new BigDecimal("0.02")));
         tendiesList.add(new Tender(getResources().getDrawable(R.drawable.ic_launcher_foreground), "Venus", 2.22,   "$", new BigDecimal("0.02")));
         tendiesList.add(new Tender(getResources().getDrawable(R.drawable.ic_launcher_foreground), "Venus", 2.22,  "$", new BigDecimal("0.02")));
